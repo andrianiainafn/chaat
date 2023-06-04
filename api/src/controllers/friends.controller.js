bb9a5e4b21bb0edfaf4d6cfe724aedc3559814e5
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2023-06-04 22:54:18 +0300

    backend friends correction

--- api/src/controllers/friends.controller.js
+++ api/src/controllers/friends.controller.js
@@ -38,8 +38,11 @@ exports.cancelFriendRequest = async(req,res)=>{
         }
         jwt.verify(token,process.env.JWT_SECRET)
         const payload = jwt.decode(token,options={"verify_signature": false})
-
-
+        const requestId = req.params.request
+        await userModel.updateOne(
+            {_id: requestId},
+            {$pull: {request: payload.user_id}}
+        )
         res
         .status(200)
         .json({message: 'Request has sent successfully'})
@@ -59,15 +62,40 @@ exports.DeleteFriendRequest = async(req,res)=>{
         }
         jwt.verify(token,process.env.JWT_SECRET)
         const payload = jwt.decode(token,options={"verify_signature": false})
-        
-
+        const requestId = req.params.request
+        await userModel.updateOne(
+            {id: payload.user_id},
+            {$pull: {request: requestId}}
+        )
         res
         .status(200)
-        .json({message: 'Request has sent successfully'})
+        .json({message: 'Delete request successfully'})
     }catch(e){
             res
             .status(500)
             .json({message:'Internal Server Error'})
+    }
+}
+exports.checkFriendRequest = async(req,res)=>{
+    try{
+        const token = req.cookies.user
+        if(!token){
+            return res
+            .status(401)
+            .json({message:"Unauthorized"})
+        }
+        jwt.verify(token,process.env.JWT_SECRET)
+        const payload = jwt.decode(token,options={"verify_signature": false})
+        const requestId = req.params.request
+        const userRequest = await userModel.findOne({_id: requestId}).select('request')
+        res
+        .status(200)
+        .json({message: userRequest})
+
+    }catch(e){
+        res
+        .status(500)
+        .json({message:'Internal Server Error'})
     }
 }
 exports.getAllFriends = async(req,res)=>{
