35453287ab9a1e533c8858e58133022eb625694c
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2023-06-13 06:59:12 +0300

    ajout des utilisateur en ligne socket

--- api/src/sockets/messsage.socket.js
+++ api/src/sockets/messsage.socket.js
@@ -1,6 +1,12 @@
+let users =[]
+const addUser = (user_id,socket_id)=>{
+    !users.some((user)=> user.user_id ===  user_id) && 
+    users.push({user_id,socket_id})
+}
 const HandleMessageChange = (io)=>{
     io.on('connect', (socket)=>{
-        socket.on('addUser', (user)=>console.log(user,"this is socket"))
+        socket.on('addUser', (user_id)=> addUser(user_id,socket.id))
+        io.emit('getuser', users)
     })
 }
 module.exports = HandleMessageChange;
