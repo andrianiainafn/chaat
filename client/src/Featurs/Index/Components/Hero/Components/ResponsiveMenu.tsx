c2e9d88895f2c84e36506021263f0c583e36cff9
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2023-07-06 19:25:20 +0300

    ajout du navbar responsive

---
+++ client/src/Featurs/Index/Components/Hero/Components/ResponsiveMenu.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {}
+
+const ResponsiveMenu = (props: Props) => {
+  return (
+    <div className='menu-parent'>
+        <div className="menu-child">
+
+        </div>
+    </div>
+  )
+}
+
+export default ResponsiveMenu
