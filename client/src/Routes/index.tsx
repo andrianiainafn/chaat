f8fd56a7ba760e7f848bd0ecb1036f8b40cf4847
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2023-07-09 21:50:19 +0300

    modification du route

--- client/src/Routes/index.tsx
+++ client/src/Routes/index.tsx
@@ -1,18 +1,27 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { useContext, useEffect } from "react";
+import { Route, Routes, redirect } from "react-router-dom";
 import Acceuille from "../Featurs/Index/Acceuille";
 import AdminLayout from "../Layout/AdminLayout";
 import AuthLayout from "../Layout/AuthLayout";
 import UsersLayout from "../Layout/UsersLayout";
+import AuthContext from "../Context/GlobalContext";
 
 
 function AppRouter(){
+  const {connected} = useContext(AuthContext)
   return (
     <Routes>
-        <Route path="/" element={<Acceuille/>} />
-        <Route path="auth/*" element={<AuthLayout/>}/>
+        {
+          !connected && (
+            <>
+              <Route path="/" element={<Acceuille/>} />
+              <Route path="auth/*" element={<AuthLayout/>}/></>
+          )
+        }
         <Route path="admin/*" element={<AdminLayout/>}/>
-        <Route path="users/*" element={<UsersLayout/>}/>
+        {
+          connected && <Route path="users/*" element={<UsersLayout/>}/>
+        }
     </Routes>
   )
 }
