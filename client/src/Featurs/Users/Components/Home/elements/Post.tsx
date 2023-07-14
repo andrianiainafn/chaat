39cd9d29fec4cf060138aaef853cc1bae3c827f4
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2023-07-14 23:27:33 +0300

    reglase du cookie

--- client/src/Featurs/Users/Components/Home/elements/Post.tsx
+++ client/src/Featurs/Users/Components/Home/elements/Post.tsx
@@ -40,12 +40,12 @@ const Post = (props: Props) => {
       }
     })
     if(response.status === 200){
+      setTest(an=>!an)
       console.log("Fa aona no tsy mety kosa eee")
         console.log(response.data,9696)
         setHasNewReaction((reaction:any)=>[...reaction,response.data])
         queryClient.invalidateQueries(['reaction',postId])
       }else{
-        setTest(an=>!an)
         console.log(response,666)
       }
     }
