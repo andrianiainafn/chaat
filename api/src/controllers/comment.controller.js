fbdab319025b509436c770dae010669fdb39ebfc
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2023-07-15 09:12:20 +0300

    rectification du reaction

--- api/src/controllers/comment.controller.js
+++ api/src/controllers/comment.controller.js
@@ -4,17 +4,18 @@ const postsModel = require('../models/post.model')
 
 exports.add = async(req,res)=>{
     try{
-        const token = req.cookies.user
-        if(!token){
-            return res
-            .status(401)
-            .json({message:"Unauthorized"})
-        }
-        jwt.verify(token,process.env.JWT_SECRET)
-        const payload = jwt.decode(token,options={"verify_signature": false})
+        // const token = req.cookies.user
+        // if(!token){
+        //     return res
+        //     .status(401)
+        //     .json({message:"Unauthorized"})
+        // }
+        // jwt.verify(token,process.env.JWT_SECRET)
+        // const payload = jwt.decode(token,options={"verify_signature": false})
+        const user_id = req.userId
         const {description,post} = req.body
         const newComment = new commentModel({
-            author: payload.user_id,
+            author: user_id,
             description,
             post
         })
--- api/src/routes/comment.router.js
+++ api/src/routes/comment.router.js
@@ -1,8 +1,9 @@
 const route = require('express').Router();
 const {add,remove,get,modify, checkreaction,reaction} = require('../controllers/comment.controller');
+const authMiddleware = require('../middelwares/auth.middelware');
 
 route.get('/get/:post', get);
-route.post('/add', add);
+route.post('/add',authMiddleware, add);
 route.get('/checkreaction/:comment', checkreaction)
 route.put('/reaction/:comment',reaction)
 route.put('/modify/:comment',modify)
