b8cb22a926c76aa6e66dd1fa2b8b411339faf723
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2023-07-15 12:57:26 +0300

    essayer de correction du bug copokie not ste domain invalid

--- api/src/middelwares/auth.middelware.js
+++ api/src/middelwares/auth.middelware.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const jwt = require('jsonwebtoken')
 
 const authMiddleware = async (req, res, next) => {
-  console.log(req.headers)
+  console.log(req.headers.authorization)
     const token = req.headers.authorization.split(' ')[1];
     if (!token) return res.status(403).json({ message: 'Forbidden' });
     try {
