bfe971e259d84fc2a6dd8d3fefd8f5531ad43be6
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2023-07-15 09:21:43 +0300

    loe be ah eee

--- api/src/controllers/comment.controller.js
+++ api/src/controllers/comment.controller.js
@@ -4,18 +4,17 @@ const postsModel = require('../models/post.model')
 
 exports.add = async(req,res)=>{
     try{
-        // const token = req.cookies.user
-        // if(!token){
-        //     return res
-        //     .status(401)
-        //     .json({message:"Unauthorized"})
-        // }
-        // jwt.verify(token,process.env.JWT_SECRET)
-        // const payload = jwt.decode(token,options={"verify_signature": false})
-        const user_id = req.userId
+        const token = req.cookies.user
+        if(!token){
+            return res
+            .status(401)
+            .json({message:"Unauthorized"})
+        }
+        jwt.verify(token,process.env.JWT_SECRET)
+        const payload = jwt.decode(token,options={"verify_signature": false})
         const {description,post} = req.body
         const newComment = new commentModel({
-            author: user_id,
+            author: payload.user_id,
             description,
             post
         })
