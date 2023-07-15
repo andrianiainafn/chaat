0c94320af9006fb93fa82dd9c34e01cf9d44b711
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2023-07-15 12:45:38 +0300

    essayer de correction du bug copokie not ste domain invalid

--- api/index.js
+++ api/index.js
@@ -40,15 +40,6 @@ app.use(express.json())
 //Connection avec La BD
 dotenv.config();
 mongoose.set('strictQuery', true)
-//function to connect on mongodb
-// async function main(){
-//     await mongoose.connect(process.env.MONGO_URL)
-// }
-// try{
-//     main()
-// }catch(err){
-//     console.log(err)
-// }
 main()
 .then(()=>{console.log('DB connected successfully');})
 .catch(err => console.log(err));
