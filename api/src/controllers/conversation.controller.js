73d79846df28a0279d70f2ad8b7b014ae1346d37
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2023-08-12 08:20:13 +0300

    auth ui modification

--- api/src/controllers/conversation.controller.js
+++ api/src/controllers/conversation.controller.js
@@ -128,23 +128,13 @@ exports.deleteconversation = async(req,res)=>{
 
 exports.getDefaultConversation = async(req,res)=>{
     try{
-        // const token = req.cookies.user
-        // if(!token){
-        //     return res
-        //     .status(401)
-        //     .json({message:"Unauthorized"})
-        // }
-        // jwt.verify(token,process.env.JWT_SECRET)
-        // const payload = jwt.decode(token,options={"verify_signature": false})
         const user_id = req.userId
         const getDefaultConversation = await messageModel.find({
             $or:[
                 {author: user_id},
                 {destination: user_id}
             ]
-        })
-        .sort({date:-1})
-        .limit(1)
+        }).sort({date:-1}).limit(1)
         res
         .status(200)
         .json({message:getDefaultConversation[0].conversation})
