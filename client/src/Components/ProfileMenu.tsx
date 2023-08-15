9ce90b57fece8542c57025b9cf4d950859402713
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2023-08-15 11:36:20 +0300

    fix the bug when logout

--- client/src/Components/ProfileMenu.tsx
+++ client/src/Components/ProfileMenu.tsx
@@ -8,6 +8,7 @@ import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined';
 import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
 import axios from 'axios';
 import { BASEURL } from './BaseLink';
+import { useCookies } from 'react-cookie';
 
 type Props = {
     showMenu: boolean,
@@ -17,12 +18,19 @@ type Props = {
 const ProfileMenu = (props: Props) => {
   const {connected,firstname,profilepicture,userId} = useContext(AuthContext)
   const navigate = useNavigate()
+  const [cookie] = useCookies()
   const HandleClickProfile = () =>{
       navigate(`/users/profile/home/${userId}`)
       props.HandleClickProfile()
   }
   const HandleClickLogout = async()=>{
-    await axios.get(`${BASEURL}/auth/logout`)
+    await axios.get(`${BASEURL}/auth/logout`,{
+      headers:
+      {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${cookie.name}`
+      }
+    })
     navigate('/')
   }
   return (
