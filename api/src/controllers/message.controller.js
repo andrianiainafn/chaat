1d353775a8dc5e5595861839fa32b2e31b8dfc19
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2023-12-02 11:40:00 +0300

    fix: fix post author null

--- api/src/controllers/message.controller.js
+++ api/src/controllers/message.controller.js
@@ -58,20 +58,12 @@ exports.getmessage = async(req,res)=>{
 }
 exports.deletemessage = async(req,res)=>{
     try{
-        // const token = req.cookies.user
-        // if(!token){
-        //     return res
-        //     .status(401)
-        //     .json({message:"Unauthorized"})
-        // }
         const messageId = req.params['message']
         const message = await messageModel.findOne({_id: messageId}).populate({
             path:'author',
             model:'user',
             select:['firstname']
         })
-        // jwt.verify(token,process.env.JWT_SECRET)
-        // const payload = jwt.decode(token,options={"verify_signature": false})
         const user_id = req.userId
         if(message._id === user_id){
             return res
